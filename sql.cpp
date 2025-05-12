5539b76568cd6d5252621033880f7d176e1e1825
Andrew Witten <awitten007@gmail.com>
2025-05-11 21:25:15 -0400

    store counters, not devices

--- sql.cpp
+++ sql.cpp
@@ -2,6 +2,7 @@
 #include "AMDTDefinitions.h"
 #include "AMDTPowerProfileApi.h"
 #include "AMDTPowerProfileDataTypes.h"
+#include "duckdb.hpp"
 #include <fmt/core.h>
 #include <iostream>
 #include "sql.hpp"
@@ -39,6 +40,82 @@ std::string DeviceTypeToString(AMDTDeviceType type) {
     }
 }
 
+std::string AggToString(AMDTPwrAggregation t) {
+    switch (t) {
+    case AMDT_PWR_VALUE_SINGLE:
+        return "single";
+    case AMDT_PWR_VALUE_CUMULATIVE:
+        return "cumulative";
+    case AMDT_PWR_VALUE_HISTOGRAM:
+        return "historgram";
+    default:
+        return "invalid";
+    }
+}
+
+std::string UnitsToSTring(AMDTPwrUnit u) {\
+    switch (u) {
+
+    case AMDT_PWR_UNIT_TYPE_COUNT:
+        return "count";
+    case AMDT_PWR_UNIT_TYPE_NUMBER:
+        return "number";
+    case AMDT_PWR_UNIT_TYPE_PERCENT:
+        return "percent";
+    case AMDT_PWR_UNIT_TYPE_RATIO:
+        return "ratio";
+    case AMDT_PWR_UNIT_TYPE_MILLI_SECOND:
+        return "millisecond";
+    case AMDT_PWR_UNIT_TYPE_JOULE:
+        return "joule";
+    case AMDT_PWR_UNIT_TYPE_WATT:
+        return "watt";
+    case AMDT_PWR_UNIT_TYPE_VOLT:
+        return "volt";
+    case AMDT_PWR_UNIT_TYPE_MILLI_AMPERE:
+        return "ampere";
+    case AMDT_PWR_UNIT_TYPE_MEGA_HERTZ:
+        return "MHZ";
+    case AMDT_PWR_UNIT_TYPE_CENTIGRADE:
+        return "celsius";
+    default:
+        return "invalid";
+    }
+}
+
+std::string CategoryToString(AMDTPwrCategory c) {
+    switch (c) {
+    case AMDT_PWR_CATEGORY_POWER:
+        return "power";
+    case AMDT_PWR_CATEGORY_FREQUENCY:
+        return "frequency";
+    case AMDT_PWR_CATEGORY_TEMPERATURE:
+        return "temperature";
+    case AMDT_PWR_CATEGORY_VOLTAGE:
+        return "voltage";
+    case AMDT_PWR_CATEGORY_CURRENT:
+        return "current";
+    case AMDT_PWR_CATEGORY_PSTATE:
+        return "pstate";
+    case AMDT_PWR_CATEGORY_CSTATES_RESIDENCY:
+        return "cstates_residency";
+    case AMDT_PWR_CATEGORY_TIME:
+        return "time";
+    case AMDT_PWR_CATEGORY_ENERGY:
+        return "energy";
+    case AMDT_PWR_CATEGORY_CORRELATED_POWER:
+        return "power";
+    case AMDT_PWR_CATEGORY_CAC:
+        return "cac";
+    case AMDT_PWR_CATEGORY_CONTROLLER:
+        return "controller";
+    case AMDT_PWR_CATEGORY_DPM:
+        return "dpm";
+    default:
+        return "invalid";
+    }
+}
+
 void InsertDevices(duckdb::Connection& conn) {
     for (int i = 1; i < AMDT_PWR_DEVICE_CNT; ++i) {
         auto type = AMDTDeviceType(i);
@@ -46,7 +123,7 @@ void InsertDevices(duckdb::Connection& conn) {
     }
 }
 
-void PrintCounters() {
+void StoreCounters(duckdb::Connection& con) {
     AMDTUInt32 num_counters;
     AMDTPwrCounterDesc* power_counter_desc;
     auto result = AMDTPwrGetSupportedCounters(&num_counters, &power_counter_desc);
@@ -55,18 +132,65 @@ void PrintCounters() {
         return;
     }
 
+    con.Query("PREPARE insert_into_counters AS INSERT OR IGNORE INTO pwr_counters VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)");
     for (int i = 0; i < num_counters; ++i) {
         auto& desc = power_counter_desc[i];
-        std::cout << "name " << desc.m_name 
-            << " description " << desc.m_description << std::endl << desc.m_devType << " "  << desc.m_deviceId << std::endl << std::endl;
-            // power_counter_desc[i].m_deviceId <<  " " << power_counter_desc[i].m_devType << std::endl;
+
+        auto res = con.Query(fmt::format("EXECUTE insert_into_counters({},'{}','{}',{},'{}',{},{},'{}','{}',{},{},'{}',{})", 
+            desc.m_counterID,
+            desc.m_name,
+            desc.m_description,
+            desc.m_deviceId,
+            DeviceTypeToString(desc.m_devType),
+            desc.m_devType,
+            desc.m_devInstanceId,
+            CategoryToString(desc.m_category),
+            AggToString(desc.m_aggregation),
+            desc.m_minValue,
+            desc.m_maxValue,
+            UnitsToSTring(desc.m_units),
+            desc.m_isParentCounter
+        ));
+        if (res->HasError()) {
+            std::cerr << res->GetError() << std::endl;
+        }
     }
 
 }
 
-
+// typedef struct AMDTPwrCounterDesc
+// {
+//     AMDTUInt32           m_counterID;       /**< Counter index */
+//     AMDTUInt32           m_deviceId;        /**< Device Id */
+//     AMDTDeviceType       m_devType;         /**< Device type- compute unit/Core/ package/ dGPU */
+//     AMDTUInt32           m_devInstanceId;   /**< Device instance id within the device type */
+//     char*                m_name;            /**< Name of the counter */
+//     char*                m_description;     /**< Description of the counter */
+//     AMDTPwrCategory      m_category;        /**< Power/Freq/Temperature */
+//     AMDTPwrAggregation   m_aggregation;     /**< Single/Histogram/Cumulative */
+//     AMDTFloat64          m_minValue;        /**< Minimum possible counter value */
+//     AMDTFloat64          m_maxValue;        /**< Maximum possible counter value */
+//     AMDTPwrUnit          m_units;           /**< Seconds/MHz/Joules/Watts/Volt/Ampere */
+//     bool                 m_isParentCounter; /**< If the counter has some child counters*/
+// } AMDTPwrCounterDesc;
 void DDL(duckdb::Connection& con) {
-    con.Query("CREATE TABLE IF NOT EXISTS devices(device_id BIGINT PRIMARY KEY, name VARCHAR);");
+    con.Query(
+        "CREATE TABLE IF NOT EXISTS pwr_counters( "
+            "counter_id BIGINT PRIMARY KEY, " 
+            "counter_name VARCHAR, " 
+            "counter_description VARCHAR, "
+            "device_id BIGINT, "
+            "device_name VARCHAR, "
+            "device_type VARCHAR, "
+            "device_instance_id BIGINT, "
+            "category VARCHAR, "
+            "aggregation VARCHAR, "
+            "minValue FLOAT, "
+            "maxValue FLOAT, "
+            "units VARCHAR, "
+            "has_child_counter BOOLEAN "
+        ") "
+    );
 
-    InsertDevices(con);
+    StoreCounters(con);
 }
